Fix for incorrect cell size calculation when using `lat_grid_size=dem`

src/monarchs/DEM/load_DEM.py
@@ -220,14 +220,14 @@ def get_xy_distance(latitudes, longitudes):
     else:
         lon_grid, lat_grid = longitudes, latitudes
     # Compute dy (North-South size of each grid cell in metres)
-    lat1_dy = lat_grid[:-1, :]  # Take all but last row
-    lat2_dy = lat_grid[1:, :]  # Take all but first row
-    dy_metres = geod.inv(lon_grid[:-1, :], lat1_dy, lon_grid[:-1, :], lat2_dy)[-1]
+    lat1_dy = lat_grid[:, :-1]  # Take all but last row
+    lat2_dy = lat_grid[:, 1:]  # Take all but first row
+    dy_metres = geod.inv(lon_grid[:, :-1], lat1_dy, lon_grid[:, :-1], lat2_dy)[-1]
 
     # Compute dx (East-West size of each grid cell in metres)
-    lon1_dx = lon_grid[:, :-1]  # Take all but last column
-    lon2_dx = lon_grid[:, 1:]  # Take all but first column
-    dx_metres = geod.inv(lon1_dx, lat_grid[:, :-1], lon2_dx, lat_grid[:, :-1])[-1]
+    lon1_dx = lon_grid[:-1, :]  # Take all but last column
+    lon2_dx = lon_grid[1:, :]  # Take all but first column
+    dx_metres = geod.inv(lon1_dx, lat_grid[:-1, :], lon2_dx, lat_grid[:-1, :])[-1]
 
     # Convert dx/dy to full-sized arrays by padding (so they match raster size)
     dy_metres = np.pad(dy_metres, ((0, 1), (0, 0)), mode='edge')  # Pad last row