Fix message string concatenation to use f-strings for numerical values so Numba works

src/monarchs/physics/snow_accumulation.py
@@ -88,8 +88,8 @@ def snowfall(cell, snow_depth, snow_rho, snow_T):
             # error handling
             if np.any(cell["Lfrac"] < -0.001):
                 message = "Lfrac before snow < 0"
-                message += (np.where(cell["Lfrac"] < -0.001))
-                message += (cell["Lfrac"])
+                message += f"{np.where(cell["Lfrac"] < -0.001)}"
+                message += f"{cell["Lfrac"]}"
                 utils.generic_error(cell, routine_name, message)
 
             cell["Lfrac"][0] = (weight1 * 0 + weight2 * cell["Lfrac"][0]) / (
@@ -97,19 +97,19 @@ def snowfall(cell, snow_depth, snow_rho, snow_T):
             )
             if np.any(cell["Lfrac"] < -0.001):
                 message = "Lfrac before hold < 0"
-                message += (np.where(lfrac_hold < -0.001))
-                message += (cell["Lfrac"])
-                message += lfrac_hold
+                message += f"{np.where(cell["Lfrac"] < -0.001)}"
+                message += f"{cell["Lfrac"]}"
+                message += f"{lfrac_hold}"
                 utils.generic_error(cell, routine_name, message)
             if np.any(cell["Sfrac"] < -0.001):
                 message = ("Sfrac before snow > 0")
-                message += (np.where(cell["Sfrac"] < -0.001))
-                message += (cell["Sfrac"])
+                message += f"{np.where(cell["Sfrac"] < -0.001)}"
+                message += f"{cell["Sfrac"]}"
                 utils.generic_error(cell, routine_name, message)
             if np.any(cell["Sfrac"] > 1.001):
                 message = "Sfrac before snow > 1"
-                message += (np.where(cell["Sfrac"] > 1.001))
-                message += (cell["Sfrac"])
+                message += f"{np.where(cell["Sfrac"] < -0.001)}"
+                message += f"{cell["Sfrac"]}"
                 utils.generic_error(cell, routine_name, message)
 
 
@@ -146,7 +146,7 @@ def snowfall(cell, snow_depth, snow_rho, snow_T):
             if np.any(lfrac_hold < -0.001):
                 w = np.where(lfrac_hold < -0.001)
                 message = "Lfrac hold < 0"
-                message += (w[0][0])
+                message += f"{w[0][0]}"
                 message += ("New Lfrac = ", lfrac_hold[: w[0][0]
                 + 2])
                 message += ("Old Lfrac = ", cell["Lfrac"][: w[0][
@@ -156,15 +156,15 @@ def snowfall(cell, snow_depth, snow_rho, snow_T):
             if np.any(sfrac_hold < -0.001):
                 w = np.where(sfrac_hold < 0)
                 message = "Sfrac hold < 0"
-                message += (w[0][0])
-                message += ("New Sfrac = ", sfrac_hold[: w[0][0] + 2])
-                message += ("Old Sfrac = ", cell["Sfrac"][: w[0][0] + 2])
+                message += f"{w[0][0]}"
+                message += f"New Sfrac = {sfrac_hold[: w[0][0] + 2]}"
+                message += f"Old Sfrac = {cell["Sfrac"][: w[0][0] + 2]}"
                 utils.generic_error(cell, routine_name, message)
 
             if np.any(sfrac_hold > 1.001):
                 message = "Sfrac hold > 1"
-                message += (np.where(sfrac_hold > 1.001))
-                message += (sfrac_hold)
+                message += f"{np.where(sfrac_hold > 1.001)}"
+                message += f"{sfrac_hold}"
                 utils.generic_error(cell, routine_name, message)
 
             cell["Sfrac"] = sfrac_hold