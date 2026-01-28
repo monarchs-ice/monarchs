a081b4aeded4a8e9423361acb41b4ddcbebd7d29
Isabelle Wicks <125266425+IWicks@users.noreply.github.com>
2026-01-28 15:49:16 +0000

    Simplify file existence check for RVf_filepath

--- src/monarchs/rock_view/load_RVf.py
+++ src/monarchs/rock_view/load_RVf.py
@@ -24,19 +24,18 @@ def load_RVf(RVf_filepath):
     """
     
     # Checking for the existence of the file and that it can be read.
-    if os.path.isfile(RVf_filepath) is True:
-        if os.access(RVf_filepath, os.R_OK):
-            RVf = np.loadtxt(RVf_filepath, dtype=np.float64, delimiter=',')
-        else:
-            raise IOError(f"The file {RVf_filepath} is not readable.")
+    if os.access(RVf_filepath, os.R_OK):
+        RVf = np.loadtxt(RVf_filepath, dtype=np.float64, delimiter=',')
+    else:
+        raise IOError(f"The file {RVf_filepath} is not readable.")
+    
+    # Checking that the RVf grid is the same shape as the model grid.
+    if RVf.shape != (num_rows, num_cols): # TO DO: Need to include intiliase_iceshelf? Or grid[num_rows, num_cols]?
+        raise ValueError("The RVf grid is the wrong shape!")
+    
+    
+    # Checking that all values in the grid are between 0 and 1.
+    if np.any((RVf<0) | (RVf>1)):
+        raise ValueError("All grid values must be between 0 and 1!")
         
-        # Checking that the RVf grid is the same shape as the model grid.
-        if RVf.shape != (num_rows, num_cols): # TO DO: Need to include intiliase_iceshelf? Or grid[num_rows, num_cols]?
-            raise ValueError("The RVf grid is the wrong shape!")
-        
-        
-        # Checking that all values in the grid are between 0 and 1.
-        if np.any((RVf<0) | (RVf>1)):
-            raise ValueError("All grid values must be between 0 and 1!")
-            
     return RVf
