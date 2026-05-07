27afc36ebd78cebe28ca9f4bbbd28e10b9764183
Jon Elsey <65968741+JonElsey@users.noreply.github.com>
2026-05-08 00:50:54 +0100

    hotfix: ensure initial solver guess uses correct # of vertical points

--- src/monarchs/physics/Numba/solver_nb.py
+++ src/monarchs/physics/Numba/solver_nb.py
@@ -98,7 +98,7 @@ def solve_firn_heateqn(cell, met_data, dt, dz, fixed_sfc=False, solver_method="h
     """
 
     N = max(50, cell["vert_grid"] // 10)
-    x = cell["firn_temperature"]
+    x = cell["firn_temperature"][:N]
     # Need to pack arguments into a single array, so we can pass them into the
     # solver (as NumbaMinpack expects arguments as a vector) - we unpack these
     # later in the heat equation solver
